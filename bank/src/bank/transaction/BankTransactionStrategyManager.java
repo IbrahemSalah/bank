5a52e91b201b53af48aeb27d43c8dad1f5e854be
Ibrahem Salah <Eng_IbrahemSalah@hotmail.com>
2023-06-20 19:08:56 -0500

    starting deposit and withdraw

---
+++ bank/src/bank/transaction/BankTransactionStrategyManager.java
@@ -0,0 +1,11 @@
+package bank.transaction;
+
+import finframework.actor.IAccount;
+import finframework.transaction.TransactionStrategyManager;
+
+public class BankTransactionStrategyManager extends TransactionStrategyManager {
+    @Override
+    public void setTransactionStrategy(IAccount account) {
+
+    }
+}
